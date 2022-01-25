95e843f8870e304144bbc36741806a024c5ba591
Osvaldo Cartagena <osvaldo.cartagena-stevens@student-cs.fr>
2022-01-25 17:02:49 +0100

    intervals created

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,27 +1,40 @@
 from Traj3D import *
 from random import *
+import numpy as np
+#uniform(-.5, .5)
 
 class node:  # Generic tree node
 
     #"Generic tree node."
     __ORIGINAL_ROT_TABLE = {
-    "AA": [35.62, 7.2, -154, ],
-    "AC": [34.4, 1.1,  143, ],
-    "AG": [27.7, 8.4,    2, ],
-    "AT": [31.5, 2.6,    0, ],
-    "CA": [34.5, 3.5,  -64, ],
-    "CC": [33.67, 2.1,  -57, ],
-    "CG": [29.8, 6.7,    0, ],
-    "CT": [27.7, 8.4,   -2, ],
-    "GA": [36.9, 5.3,  120, ],
-    "GC": [40, 5,  180, ],
-    "GG": [33.67, 2.1,   57, ],
-    "GT": [34.4, 1.1, -143, ],
-    "TA": [36, 0.9,    0, ],
-    "TC": [36.9, 5.3, -120, ],
-    "TG": [34.5, 3.5,   64, ],
-    "TT": [35.62, 7.2,  154, ]}  
-    
+        "AA": [35.62, 7.2, -154,      0.06,  0.6, 0],
+        "AC": [34.4, 1.1,  143,      1.3,  5, 0],
+        "AG": [27.7, 8.4,    2,      1.5,  3, 0],
+        "AT": [31.5, 2.6,    0,      1.1,  2, 0],
+        "CA": [34.5, 3.5,  -64,      0.9, 34, 0],
+        "CC": [33.67, 2.1,  -57,      0.07,  2.1, 0],
+        "CG": [29.8, 6.7,    0,      1.1,  1.5, 0],
+        "CT": [27.7, 8.4,   -2,      1.5,  3, 0],
+        "GA": [36.9, 5.3,  120,      0.9,  6, 0],
+        "GC": [40, 5,  180,      1.2,  1.275, 0],
+        "GG": [33.67, 2.1,   57,      0.07,  2.1, 0],
+        "GT": [34.4, 1.1, -143,      1.3,  5, 0],
+        "TA": [36, 0.9,    0,      1.1,  2, 0],
+        "TC": [36.9, 5.3, -120,      0.9,  6, 0],
+        "TG": [34.5, 3.5,   64,      0.9, 34, 0],
+        "TT": [35.62, 7.2,  154,      0.06,  0.6, 0]
+        }
+    nucleotidlist=["AA","AC","AG","AT","CA","CC","CG","CT","GA","GC","GG","GT","TA","TC","TG","TT"]
+    __ORIGINAL_INTERVALS={}
+    for key in __ORIGINAL_ROT_TABLE:
+        __ORIGINAL_INTERVALS[key]=  [ 
+                                    [__ORIGINAL_ROT_TABLE[key][0]-np.sqrt(3)*__ORIGINAL_ROT_TABLE[key][3],
+                                     __ORIGINAL_ROT_TABLE[key][0]+np.sqrt(3)*__ORIGINAL_ROT_TABLE[key][3] ],
+                                    [__ORIGINAL_ROT_TABLE[key][1]-np.sqrt(3)*__ORIGINAL_ROT_TABLE[key][4],
+                                    __ORIGINAL_ROT_TABLE[key][1]-np.sqrt(3)*__ORIGINAL_ROT_TABLE[key][4]],
+                                    [__ORIGINAL_ROT_TABLE[key][2]-np.sqrt(3)*__ORIGINAL_ROT_TABLE[key][5],
+                                    __ORIGINAL_ROT_TABLE[key][2]-np.sqrt(3)*__ORIGINAL_ROT_TABLE[key][5]]
+                                    ]
     # Exemple de représentation
     # """  Use a  tree to search in protein folding
     # We have 2 class: a tree and nodes
@@ -60,9 +73,9 @@ class node:  # Generic tree node
 
     # """
 
-    def __init__(self):
+    def __init__(self,table):
 
-        self.__Rot_Table={} #to complete
+        self.__Rot_Table=table #to complete
 
         
         self.__valeur=0     #to complete
@@ -91,8 +104,14 @@ class node:  # Generic tree node
 
     def getvalue(self):
         return self.__valeur
+
+    def getTable(self):
+        return self.__Rot_Table
     
     def actualizeN(self):
+        pass
+    
+    def shuffle(self):
         pass
 
 
